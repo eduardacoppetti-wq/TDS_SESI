32ded202d8606dfa50836287f8b1e7b59b71a011
EduardaCoppetti <eduarda.coppetti@escola.pr.gov.br>
2026-02-26 12:13:24 -0300

    Inicio com JS

---
+++ inicio/desafio_blocos.js
@@ -0,0 +1,16 @@
+/*
+|  Desafio: Criar dois blocos de códigos.
+|  Onde o primeiro deve imprimir na tela os
+|  números pares de 0 a 10 e o segundo
+|  bloco deve imprimir na tela os impares
+*/
+
+{
+    console.log("números pares");
+    console.log("0,2,4,6,8,10");
+}
+   
+{ 
+    console.log("números ímpares");
+    console.log("1,3,5,7,9");
+}
