1554c597faddb9481f401406f77f496e1dce6ae8
EduardaCoppetti <eduarda.coppetti@escola.pr.gov.br>
2026-02-27 11:53:37 -0300

    tipos de dados em js

---
+++ inicio/dados2.js
@@ -0,0 +1,14 @@
+console.log(19.9 * 0.6);
+
+var preco = 19.9;
+var desconto = 0.6;
+var precoComDesconto = preco * (1 - desconto);
+console .log(precoComDesconto);
+
+
+let nome = "caderno";
+let categoria = "papelaria";
+console.log("Produto: " + nome + ", categoria; " + categoria + " , Preco: " + preco + " , Desconto: " + desconto);
+
+
+
