5d62f8bdb6de03fd822f4ae00908a8beab96610f
EduardaCoppetti <eduarda.coppetti@escola.pr.gov.br>
2026-03-06 11:39:29 -0300

    operadores de atribuicao e relacionais

---
+++ operadores/desafios_relacionais.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 11;
+
+// Imprimir npo console S numeros verdadeiros(true)
+console.log(a < b); 
+console.log(a <= b);
+console.log(b > a);
+console.log(a != b);
+
+
+// imprimir no console 5 numeros falsos(false)
+console.log(a > b);
+console.log(a >= b);
+console>log(a == b);
+console.log(a === b);
---
+++ operadores/racionais2.js
@@ -0,0 +1,18 @@
+let a = 1;
+let b = "1"; // string
+let c = "1"; // string
+
+// = atribuicao
+// == compara somente valor
+// === compara valor e tipo
+
+console.log(a == b);
+console.log(a === b);
+console.log(a === c);
+
+console.log(a != b);
+console.log(a !== b);
+console.log(b !== b);
+
+
+
