5e248f95cd13aa893ba3d73838a32b2bc8e0362a
EduardaCoppett <eduarda.coppetti@escola.pr.gov.br>
2026-03-12 12:12:42 -0300

    ESTRUTURAS DE CONTROLE

---
+++ operadores/controle/else_if.js
@@ -0,0 +1,10 @@
+// Desafio
+
+let nota = 8;
+if (nota >= 7) {
+console.log("aprovado");
+} else if(nota < 7 && nota > 5) {
+    console.log("Recuperacao");
+} else {
+     console.log ("Reprovado");
+}
---
+++ operadores/controle/template_string.js
@@ -0,0 +1,6 @@
+let nome = "Eduarda Rafaela";
+let idade = 16;
+// imprimir no terminal o nome e  idade
+// Junto com uma frASE.
+console.log("Meu nome e " + nome + " e tenho " + idade + " anos de idade ");
+console.log('Meu nome e $(nome) e tenho  $(idade)  anos de idade.' );
