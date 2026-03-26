e62a4f2ad0a090b5aea53b2aebe2bc1b6995663e
EduardaCoppetti <eduarda.coppetti@escola.pr.gov.br>
2026-03-26 11:33:11 -0300

    atividade 1

---
+++ objetos/atividades/exemplo commit.js
@@ -0,0 +1,9 @@
+var produto{
+let nome = caneta
+let estoque = 100
+let preco = 2,00
+// imprimir no terminal o nome, estoque e preco
+// Junto com uma frASE.
+
+}
+console.log("O produto " + nome + " tem " + estoque + " unidades no estoque e ele custa + preco + $ 2,00");
