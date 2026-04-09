31439c653fba999cf19a52092c81ce444e47ccab
EduardaCoppetti <eduarda.coppetti@escola.pr.gov.br>
2026-04-09 11:57:47 -0300

    atividades_final.js

---
+++ array/atividades_final.js/atividade1.js
@@ -0,0 +1,23 @@
+const componentes = ["culer", "monitor", "mouse", "placa de video, Memoria Ram"];
+
+componentes.shift(componentes[0]);
+componentes.push("CPU");
+
+console.log(componentes[0]);
+console.log(componentes[1]);
+console.log(componentes[2]);
+console.log(componentes[3]);
+console.log(componentes);
+
+const clienteNome = {
+    nome : "Eduarda",
+    possuicupom : true
+}
+
+const clienteIdade = {
+    nome : "16" ,
+    possuicupom : true
+}
+console.log
+
+
